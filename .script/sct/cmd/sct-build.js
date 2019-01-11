944f337ed10e5fcc07611f3d8ec45a4a0437cbf9
Ethan Pini <epini@sfu.ca>
2019-01-11 02:52:01 -0800

    tools: Add stub for 'sct build'

---
+++ .script/sct/cmd/sct-build.js
@@ -0,0 +1,44 @@
+#!/usr/bin/env node
+// ---------------------------------------------------------------------------------------------------------------------
+// Copyright (C) 2019 Ethan Pini <epini@sfu.ca>
+// MIT License
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+// SCT Build: Build the project.
+// This tool will build the project.
+// ---------------------------------------------------------------------------------------------------------------------
+'use strict';
+
+// Modules.
+const Command = require('@sct').Command;
+
+// ---------------------------------------------------------------------------------------------------------------------
+// Class:
+// ---------------------------------------------------------------------------------------------------------------------
+
+module.exports = class CommandBuild extends Command {
+
+	name() {
+		return 'sct build';
+	}
+
+	description() {
+		return ' Build the project.';
+	}
+
+	schema() {
+		return {
+			'release': {
+				type: 'boolean',
+				default: false,
+				description: 'Build for release.',
+			}
+		}
+	}
+
+	async run(args) {
+		console.error('This subcommand is a stub.');
+		console.error('It will be implemented in a later version.');
+		return 0;
+	}
+
+};
