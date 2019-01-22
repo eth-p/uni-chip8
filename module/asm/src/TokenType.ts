625ae0bcc40f1fe5c0ee339079ecb97997227f80
Ethan Pini <epini@sfu.ca>
2019-01-22 01:35:26 -0800

    asm: Add assembly lexer

---
+++ module/asm/src/TokenType.ts
@@ -0,0 +1,72 @@
+//! --------------------------------------------------------------------------------------------------------------------
+//! Copyright (C) 2019 Team Chipotle
+//! MIT License
+//! --------------------------------------------------------------------------------------------------------------------
+// ---------------------------------------------------------------------------------------------------------------------
+
+/**
+ * The type of a source code token.
+ */
+enum TokenType {
+	/**
+	 * A label definition.
+	 */
+	LABEL,
+
+	/**
+	 * A constant parameter.
+	 */
+	PARAMETER_CONSTANT,
+
+	/**
+	 * A register parameter.
+	 */
+	PARAMETER_REGISTER,
+
+	/**
+	 * A label parameter.
+	 */
+	PARAMETER_LABEL,
+
+	/**
+	 * A comment.
+	 */
+	COMMENT,
+
+	/**
+	 * An instruction invocation.
+	 */
+	INSTRUCTION,
+
+	/**
+	 * A compile directive invocation.
+	 */
+	DIRECTIVE,
+
+	/**
+	 * Miscellaneous syntax.
+	 */
+	SYNTAX,
+
+	/**
+	 * Miscellaneous whitespace.
+	 */
+	WHITESPACE,
+
+	/**
+	 * Sprite token.
+	 */
+	SPRITE,
+
+	/**
+	 * Unknown token.
+	 */
+	UNKNOWN,
+
+	/**
+	 * End of line.
+	 */
+	EOL
+}
+
+export default TokenType;
