85cc98ee4c58bad2db8d1b81a23802d85a835b96
Ethan Pini <epini@sfu.ca>
2019-01-22 10:41:17 -0800

    types: Add Bitmask

---
+++ module/types/src/Bitmask.ts
@@ -0,0 +1,154 @@
+//! --------------------------------------------------------------------------------------------------------------------
+//! Copyright (C) 2019 Team Chipotle
+//! MIT License
+//! --------------------------------------------------------------------------------------------------------------------
+import assert from './assert';
+
+import Bitfield from './Bitfield';
+import {default as Uint, bitscanf, bitscanr, bitshiftl, bitshiftr, MIN, MAX, BITS} from './Uint16';
+// ---------------------------------------------------------------------------------------------------------------------
+
+/**
+ * A bitwise mask.
+ * Only supports up to 16 bits.
+ */
+export default class Bitmask {
+	// -------------------------------------------------------------------------------------------------------------
+	// | Fields:                                                                                                   |
+	// -------------------------------------------------------------------------------------------------------------
+
+	/**
+	 * The bitmask.
+	 */
+	public readonly mask: Uint;
+
+	/**
+	 * The index of the least-significant (rightmost) bit in the mask.
+	 */
+	public readonly lsb: Uint;
+
+	/**
+	 * The index of the most-significant (leftmost) bit in the mask.
+	 */
+	public readonly msb: Uint;
+
+	/**
+	 * The width of the bitmask.
+	 * This is the the difference between the MSB and LSB.
+	 */
+	public readonly width: Uint;
+
+	// -------------------------------------------------------------------------------------------------------------
+	// | Constructor:                                                                                              |
+	// -------------------------------------------------------------------------------------------------------------
+
+	/**
+	 * Creates a new bitmask.
+	 *
+	 * @param mask The mask.
+	 */
+	public constructor(mask: Uint) {
+		assert(mask >= MIN && mask <= MAX, "Parameter 'mask' is out of range");
+
+		this.mask = mask;
+		this.lsb = mask === 0 ? 0 : bitscanf(mask);
+		this.msb = mask === 0 ? 0 : bitscanr(mask);
+		this.width = this.msb - this.lsb;
+
+		assert(this.width >= 0, 'Bitmask width is negative?');
+	}
+
+	// -------------------------------------------------------------------------------------------------------------
+	// | Methods:                                                                                                  |
+	// -------------------------------------------------------------------------------------------------------------
+
+	/**
+	 * Extracts and shifts the masked value from a number.
+	 * This will shift the extracted value to align the LSB.
+	 *
+	 * @param from The number to extract from.
+	 * @returns The extracted value.
+	 */
+	public extract(from: Uint): Uint {
+		return this.mask === 0 ? 0 : bitshiftr(from & this.mask, this.lsb);
+	}
+
+	/**
+	 * Emplaces a masked value into a number.
+	 * This will automatically shift the value to align.
+	 *
+	 * @param value The value to emplace.
+	 * @param to The number.
+	 */
+	public emplace(value: Uint, to: Uint): Uint {
+		return this.mask === 0 ? to : (to & ~this.mask) | bitshiftl(value, this.lsb);
+	}
+
+	/**
+	 * Checks if the bitmask overlaps any values/bitmasks.
+	 *
+	 * @param value The value(s) to check.
+	 * @returns True if overlapping.
+	 */
+	public overlaps(value: Uint | Bitmask | Uint[] | Bitmask[] | (Bitmask | Uint)[]): boolean {
+		let values = value instanceof Array ? value : [value];
+
+		for (let value of values) {
+			if (value instanceof Bitmask) {
+				if ((this.mask & value.mask) > 0) return true;
+			} else {
+				if ((this.mask & value) > 0) return true;
+			}
+		}
+
+		return false;
+	}
+
+	// -------------------------------------------------------------------------------------------------------------
+	// | Casting:                                                                                                  |
+	// -------------------------------------------------------------------------------------------------------------
+
+	/**
+	 * Creates a string from the bitmask.
+	 * @returns A string representing the bitmask.
+	 */
+	public toString(): string {
+		return this.mask.toString();
+	}
+
+	/**
+	 * Creates a number from the bitmask.
+	 * @returns A number representing the bitmask.
+	 */
+	public toNumber(): number {
+		return this.mask;
+	}
+
+	/**
+	 * Creates a bitfield from the bitmask.
+	 *
+	 * @returns A corresponding bitfield.
+	 */
+	public toBitfield(): Bitfield {
+		return Bitfield.from(this.mask, BITS);
+	}
+
+	/**
+	 * Creates a primitive type.
+	 * This is a JS engine hook.
+	 *
+	 * @param hint The primitive type to covert to.
+	 * @override
+	 */
+	public [Symbol.toPrimitive](hint: string): Exclude<any, undefined | null> {
+		switch (hint) {
+			case 'number':
+				return this.mask;
+
+			case 'string':
+			case 'default':
+			default:
+				return this.toString();
+		}
+	}
+}
---
+++ module/types/test/Bitmask.test.ts
@@ -0,0 +1,110 @@
+//! --------------------------------------------------------------------------------------------------------------------
+//! Copyright (C) 2019 Team Chipotle
+//! MIT License
+//! --------------------------------------------------------------------------------------------------------------------
+import Bitfield from '../src/Bitfield';
+import Bitmask from '../src/Bitmask';
+// ---------------------------------------------------------------------------------------------------------------------
+describe('Bitmask', () => {
+	it('constructor', () => {
+		let bm = new Bitmask(0b10000001);
+		expect(bm.mask).toStrictEqual(0b10000001);
+		expect(bm.lsb).toStrictEqual(0);
+		expect(bm.msb).toStrictEqual(7);
+		expect(bm.width).toStrictEqual(7);
+
+		let bm2 = new Bitmask(0b01000010);
+		expect(bm2.mask).toStrictEqual(0b01000010);
+		expect(bm2.lsb).toStrictEqual(1);
+		expect(bm2.msb).toStrictEqual(6);
+		expect(bm2.width).toStrictEqual(5);
+
+		let bm3 = new Bitmask(0b1000000001000010);
+		expect(bm3.mask).toStrictEqual(0b1000000001000010);
+		expect(bm3.lsb).toStrictEqual(1);
+		expect(bm3.msb).toStrictEqual(15);
+		expect(bm3.width).toStrictEqual(14);
+	});
+
+	it('extract', () => {
+		let bm = new Bitmask(0b00110000);
+		expect(bm.extract(0xff)).toStrictEqual(0b11);
+
+		let bm2 = new Bitmask(0b11010000);
+		expect(bm2.extract(0xff)).toStrictEqual(0b1101);
+	});
+
+	it('emplace', () => {
+		let bm = new Bitmask(0b00110000);
+		expect(bm.emplace(0b10, 0x00)).toStrictEqual(0b00100000);
+		expect(bm.emplace(0b01, 0x00)).toStrictEqual(0b00010000);
+		expect(bm.emplace(0b11, 0x01)).toStrictEqual(0b00110001);
+		expect(bm.emplace(0b00, 0xff)).toStrictEqual(0b11001111);
+	});
+
+	it('overlaps', () => {
+		let bm = new Bitmask(0b00110000);
+		let bmoT = new Bitmask(0b00100000);
+		let bmoF = new Bitmask(0b01000000);
+
+		expect(bm.overlaps(bmoT)).toStrictEqual(true);
+		expect(bm.overlaps([bmoT, bmoT])).toStrictEqual(true);
+		expect(bm.overlaps([bmoF, bmoT, bmoF])).toStrictEqual(true);
+		expect(bm.overlaps([bmoF, 0xff])).toStrictEqual(true);
+		expect(bm.overlaps(0xff)).toStrictEqual(true);
+
+		expect(bm.overlaps(bmoF)).toStrictEqual(false);
+		expect(bm.overlaps([bmoF, bmoF])).toStrictEqual(false);
+		expect(bm.overlaps([bmoF, 0x00])).toStrictEqual(false);
+		expect(bm.overlaps(0x00)).toStrictEqual(false);
+	});
+
+	it('toString', () => {
+		let bm = new Bitmask(0b00110000);
+		expect(bm.toString()).toStrictEqual('48');
+	});
+
+	it('toNumber', () => {
+		let bm = new Bitmask(0b00110000);
+		expect(bm.toNumber()).toStrictEqual(48);
+	});
+
+	it('toBitfield', () => {
+		let bm = new Bitmask(0b00110000);
+		let bf = bm.toBitfield();
+		expect(bf).toBeInstanceOf(Bitfield);
+		expect(Array.from(bf)).toEqual([
+			false,
+			false,
+			false,
+			false,
+			false,
+			false,
+			false,
+			false,
+			false,
+			false,
+			true,
+			true,
+			false,
+			false,
+			false,
+			false
+		]);
+	});
+
+	it('[Symbol.toPrimitive] (Number)', () => {
+		let bm = new Bitmask(0b00110000);
+		expect(+bm).toStrictEqual(48);
+	});
+
+	it('[Symbol.toPrimitive] (String)', () => {
+		let bm = new Bitmask(0b00110000);
+		expect(`${bm}`).toStrictEqual('48');
+	});
+
+	it('[Symbol.toPrimitive] (Default)', () => {
+		let bm = new Bitmask(0b00110000);
+		expect(bm + '').toStrictEqual('48');
+	});
+});
