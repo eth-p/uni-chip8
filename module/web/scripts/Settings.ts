f94deddb277515131b0db7b15d7ed9acf9226bbb
Ethan Pini <epini@sfu.ca>
2019-01-25 21:02:58 -0800

    web: Refactor Settings to use Emitter, add validation

--- module/web/scripts/Settings.ts
+++ module/web/scripts/Settings.ts
@@ -2,13 +2,28 @@
 //! Copyright (C) 2019 Team Chipotle
 //! MIT License
 //! --------------------------------------------------------------------------------------------------------------------
+import Emitter from '@chipotle/types/Emitter';
+
+//! --------------------------------------------------------------------------------------------------------------------
 
 /**
  * A decorator for specifying a configurable setting.
  */
-function Setting(value: any) {
+function Setting(value: any, options?: SettingsEntry) {
 	return function(target: any, property: string | symbol) {
-		target.constructor.defaults.set(property, value);
+		target.constructor.entries.set(
+			property,
+			Object.assign(
+				{
+					validator: null
+				},
+				options,
+				{
+					value: value,
+					name: property
+				}
+			)
+		);
 	};
 }
 
@@ -16,44 +31,37 @@ function Setting(value: any) {
 // Settings:
 // ---------------------------------------------------------------------------------------------------------------------
 
-type SettingsListener = (setting: string, value: any, settings: Settings) => void;
-
 /**
  * An object for storing settings in localStorage.
  * This also provides an interface for listening for changes to the settings.
  */
-class Settings {
+class Settings extends Emitter {
 	// -------------------------------------------------------------------------------------------------------------
 	// | Fields:                                                                                                   |
 	// -------------------------------------------------------------------------------------------------------------
 
-	private static defaults: Map<string, any> = new Map();
-	private listeners: SettingsListener[];
-	private namespace: string;
+	protected static entries: Map<string, any> = new Map();
+	protected _namespace: string;
 
 	// -------------------------------------------------------------------------------------------------------------
 	// | Constructor:                                                                                              |
 	// -------------------------------------------------------------------------------------------------------------
 
 	public constructor(namespace: string) {
-		this.listeners = [];
-		this.namespace = namespace;
+		super();
+		this._namespace = namespace;
 
 		// Create properties.
-		for (let [prop, value] of (<any>this.constructor).defaults.entries()) {
+		for (let {name, value} of this.getEntries()) {
 			Object.defineProperties(this, {
-				[prop]: {
+				[name]: {
 					enumerable: true,
 					configurable: false,
-					get: () => {
-						return this.get(<string>prop);
-					},
-					set: val => {
-						this.set(<string>prop, val);
-					}
+					get: () => this.get(name),
+					set: val => this.set(name, val)
 				},
 
-				[`_${prop}`]: {
+				[`_${name}`]: {
 					enumerable: false,
 					configurable: false,
 					writable: true,
@@ -71,20 +79,53 @@ class Settings {
 	// -------------------------------------------------------------------------------------------------------------
 
 	/**
-	 * Gets the setting keys.
+	 * Gets the settings keys.
+	 * @returns An array of valid settings keys.
 	 */
-	public get keys(): string[] {
-		return Array.from((<any>this.constructor).defaults.keys());
+	public getKeys(): string[] {
+		return Array.from((<any>this.constructor).entries.keys());
+	}
+
+	/**
+	 * Gets a settings entry.
+	 * @param setting The setting key.
+	 */
+	public getEntry(setting: string): SettingsEntry | null {
+		let entry = (<any>this.constructor).entries.get(setting);
+		if (entry === undefined) return null;
+		return entry;
+	}
+
+	/**
+	 * Gets the settings entry descriptors.
+	 * @returns An array of settings descriptors.
+	 */
+	public getEntries(): SettingsEntry[] {
+		return (<any>this.constructor).entries.values();
 	}
 
 	/**
 	 * Sets a setting.
 	 * @param setting The setting to set.
 	 * @param value The value.
+	 * @returns True or false, depending on whether or not the data provided was valid.
 	 */
-	public set(setting: string, value: any): void {
+	public set(setting: string, value: any): boolean {
+		let entry = this.getEntry(setting);
+
+		// Validate.
+		if (entry === null) return false;
+		if (entry.validator != null) {
+			if (!entry.validator(value)) {
+				this.emit('invalid', setting, value);
+				return false;
+			}
+		}
+
+		// Set.
 		(<any>this)[`_${setting}`] = value;
-		this.listeners.forEach((listener: any) => listener(setting, value, this));
+		this.emit('update', setting, value);
+		return true;
 	}
 
 	/**
@@ -99,36 +140,38 @@ class Settings {
 	}
 
 	/**
-	 * Adds a settings listener.
-	 * This will be called whenever a setting changes.
-	 *
-	 * @param listener The listener function.
-	 */
-	public addListener(listener: (setting: string, value: any) => void): void {
-		this.listeners.push(listener);
-	}
-
-	/**
 	 * Loads the settings from localStorage.
 	 */
 	public load(): void {
-		let settings = localStorage.getItem(`${this.namespace}/settings`);
+		let settings = localStorage.getItem(`${this._namespace}/settings`);
 		let json;
 
+		// Attempt to parse the JSON from localStorage.
 		try {
 			json = JSON.parse(settings!);
-			if (json == null) json = {};
+			if (json == null || typeof json !== 'object') json = {};
 		} catch (ex) {
 			json = {};
 		}
 
-		for (let setting of this.keys) {
-			if (json[setting] == null) {
-				this.set(setting, (<any>this.constructor).defaults.get(setting));
-			} else {
-				this.set(setting, json[setting]);
+		// Attempt to use the values from the JSON.
+		for (let entry of this.getEntries()) {
+			let {name, value, validator} = entry;
+			let jsonValue = json[name];
+			if (
+				jsonValue == null ||
+				typeof jsonValue !== typeof value ||
+				(validator != null && !validator(jsonValue))
+			) {
+				this.set(name, value);
+				continue;
 			}
+
+			this.set(name, jsonValue);
 		}
+
+		// Done.
+		this.emit('load');
 	}
 
 	/**
@@ -137,11 +180,14 @@ class Settings {
 	public save(): void {
 		let json: any = {};
 
-		for (let setting of this.keys) {
+		// Write to JSON.
+		for (let setting of this.getKeys()) {
 			json[setting] = (<any>this)[setting];
 		}
 
-		localStorage.setItem(`${this.namespace}/settings`, JSON.stringify(json));
+		// Write to localStorage.
+		localStorage.setItem(`${this._namespace}/settings`, JSON.stringify(json));
+		this.emit('save');
 	}
 
 	/**
@@ -149,12 +195,29 @@ class Settings {
 	 * This will not save them.
 	 */
 	public reset(): void {
-		for (let setting of this.keys) {
-			this.set(setting, (<any>this.constructor).defaults.get(setting));
+		for (let setting of this.getEntries()) {
+			this.set(setting.name, setting.value);
 		}
 	}
 }
 
+interface SettingsEntry {
+	/**
+	 * A function to validate the settings value.
+	 */
+	validator: (value: any) => boolean;
+
+	/**
+	 * The name of the setting.
+	 */
+	name: string;
+
+	/**
+	 * The default value of the setting.
+	 */
+	value: any;
+}
+
 // ---------------------------------------------------------------------------------------------------------------------
 export default Settings;
-export {Settings, SettingsListener, Setting};
+export {Settings, SettingsEntry, Setting};
