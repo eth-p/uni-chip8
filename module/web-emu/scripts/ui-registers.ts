693fb9160b6582119748691030641c9a8ba8b012
Ethan Pini <epini@sfu.ca>
2019-01-27 00:21:09 -0800

    web-emu: Implement register visualizer

--- module/web-emu/scripts/ui-registers.ts
+++ module/web-emu/scripts/ui-registers.ts
@@ -2,12 +2,21 @@
 //! Copyright (C) 2019 Team Chipotle
 //! MIT License
 //! --------------------------------------------------------------------------------------------------------------------
+import UIAnimator from '@chipotle/web/UIAnimator';
 import dom_ready from '@chipotle/web/dom_ready';
 import settings from './settings';
+import {emulator, vm} from './instance';
 // ---------------------------------------------------------------------------------------------------------------------
 // Variables:
 // ---------------------------------------------------------------------------------------------------------------------
+let animator: UIAnimator<any>;
 let element: HTMLElement;
+
+let display_register_Vx: HTMLElement[];
+let display_register_ST: HTMLElement;
+let display_register_DT: HTMLElement;
+let display_register_I: HTMLElement;
+let display_register_PROGRAM: HTMLElement;
 
 // ---------------------------------------------------------------------------------------------------------------------
 // Exports:
@@ -20,21 +29,28 @@ let element: HTMLElement;
 export function refresh() {
 	if (!settings.show_registers) return;
 
-	// TODO
+	display_register_PROGRAM.innerText = vm.program_counter.toString(16).padStart(4, '0');
+	display_register_I.innerText = vm.register_index.toString(16).padStart(4, '0');
+	display_register_ST.innerText = vm.register_sound.toString(16).padStart(2, '0');
+	display_register_DT.innerText = vm.register_timer.toString(16).padStart(2, '0');
+
+	for (let i = 0, n = display_register_Vx.length; i < n; i++) {
+		display_register_Vx[i].innerText = vm.register_data[i].toString(16).padStart(2, '0');
+	}
 }
 
 /**
  * Hides the component.
  */
 export function hide(): void {
-	element.classList.add('hide');
+	setVisible(true);
 }
 
 /**
  * Shows the component.
  */
 export function show(): void {
-	element.classList.remove('hide');
+	setVisible(false);
 }
 
 /**
@@ -43,9 +59,11 @@ export function show(): void {
  */
 export function setVisible(visible: boolean): void {
 	if (visible) {
-		show();
+		animator.setCriteria('visible', true);
+		element.classList.remove('hide');
 	} else {
-		hide();
+		animator.setCriteria('visible', false);
+		element.classList.add('hide');
 	}
 }
 
@@ -54,9 +72,39 @@ export function setVisible(visible: boolean): void {
 // ---------------------------------------------------------------------------------------------------------------------
 dom_ready(() => {
 	element = <HTMLElement>document.querySelector('#emulator-registers')!;
+
+	display_register_PROGRAM = <HTMLElement>(
+		document.querySelector('[data-visualizer="register-PROGRAM"] .register-value')
+	);
+	display_register_I = <HTMLElement>document.querySelector('[data-visualizer="register-I"] .register-value');
+	display_register_ST = <HTMLElement>document.querySelector('[data-visualizer="register-ST"] .register-value');
+	display_register_DT = <HTMLElement>document.querySelector('[data-visualizer="register-DT"] .register-value');
+	display_register_Vx = (<HTMLElement[]>Array.from(document.querySelectorAll('[data-visualizer]')))
+		.map(el => [el, el.getAttribute('data-visualizer')!])
+		.filter(([el, reg]) => (<string>reg).startsWith('register-V'))
+		.map(([el, reg]) => [el, parseInt((<string>reg).substring('register-V'.length), 16)])
+		.sort(([el1, reg1], [el2, reg2]) => <number>reg1 - <number>reg2)
+		.map(([el, reg]) => <HTMLElement>(<HTMLElement>el).querySelector('.register-value'));
+
+	animator = new UIAnimator(refresh, {visible: false, debugging: false, running: false});
+	animator.resume();
 });
 
 settings.addListener('update', (setting, value) => {
-	if (setting !== 'show_registers') return;
-	setVisible(value);
+	if (setting === 'show_registers') {
+		setVisible(value);
+		return;
+	}
+
+	if (setting === 'enable_debugger') {
+		animator.setCriteria('debugging', value);
+	}
+});
+
+emulator.addListener('pause', () => {
+	animator.setCriteria('running', false);
+});
+
+emulator.addListener('resume', () => {
+	animator.setCriteria('running', true);
 });
