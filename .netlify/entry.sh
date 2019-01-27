7f919ec8d7ef3e8d6d76722fbf38ff18e31c1134
Ethan Pini <epini@sfu.ca>
2019-01-27 04:36:03 -0800

    ci: Fix Netlify attempt #2

--- .netlify/entry.sh
+++ .netlify/entry.sh
@@ -7,13 +7,19 @@
 # This is... unfortunately needed. Netlify doesn't play nicely with git-lfs.
 # ----------------------------------------------------------------------------------------------------------------------
 # Fix LFS:
+echo "Fixing LFS files..."
+[[ "$(git remote -v | wc -l)" -eq 0 ]] && {
+	git remote add origin "https://github.com/eth-p/SFU-CMPT276.git"
+}
+
 git lfs install --local
-git checkout .
+git checkout . || exit $?
 
 # Fix SCT:
 mkdir -p ".tmp" &>/dev/null
 touch ".tmp/sct-setup" &>/dev/null
 
 # Run.
+echo "Running build scripts..."
 "$@"
 exit $?
