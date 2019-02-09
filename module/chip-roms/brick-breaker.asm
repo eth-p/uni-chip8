6520799f385e66a6e7ae563891f17cd050329779
Kyle Saburao <ksaburao@sfu.ca>
2019-02-08 16:46:28 -0800

    chip-roms: Update brick breaker

--- module/chip-roms/brick-breaker.asm
+++ module/chip-roms/brick-breaker.asm
@@ -1,36 +1,67 @@
-; Reserve V0 for CHIP-8 use
-
 define BALL_X VA
 define BALL_Y VB
-define PADDLE_X VC ; This is the left-most x position of the paddle.
+define PADDLE_X VC
+define PADDLE_Y #1F
+define PADDLE_X_OLD VD
+define LEFT_KEY #1
+define RIGHT_KEY #2
+define ONE #1
 
-; Initialize the game to a start state
 init:
-
-	jp loop
-
+	LD PADDLE_X, #1F
+	LD PADDLE_X_OLD, PADDLE_X
+	LD I, sprite_paddle
+	LD V0, PADDLE_Y
+	DRW PADDLE_X, V0, ONE
+	JP loop
 
 loop:
+	CALL input_paddle
+	CALL render_paddle
 
-	jp loop
+	JP loop
 
-
-; Expect that the call stack has been cleared before jumping to reset
 reset:
-	jp init
 
+	JP init
 
-; 1 pixel wide
-ball:
-	db
-	#80
+input_paddle:
+	
+	LD PADDLE_X_OLD, PADDLE_X
 
-; 4 pixels wide
-paddle:
+	check_left_key:
+		LD V0, LEFT_KEY
+		SKP V0
+		JP check_right_key
+		LD V0, ONE
+		SUB PADDLE_X, V0
+
+	check_right_key:
+		LD V0, RIGHT_KEY
+		SKP V0
+		JP player_input_exit
+		ADD PADDLE_X, ONE
+
+	player_input_exit:
+		RET
+
+render_paddle:
+	LD I, sprite_paddle
+	LD V0, PADDLE_Y
+	SE PADDLE_X, PADDLE_X_OLD
+	JP draw_new_paddle
+
+	clear_old_paddle:
+		DRW PADDLE_X_OLD, V0, ONE
+
+	draw_new_paddle:	
+		DRW PADDLE_X, V0, ONE
+	
+	render_paddle_exit:
+		RET
+
+; SPRITES --------------------------------------------------
+
+sprite_paddle:
 	db
 	#f0
-
-; 3 pixels wide
-target:
-	db
-	#e0
