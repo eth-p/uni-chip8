6b982514a4a92c4190f7d48df55b60e0221e82de
Ethan Pini <epini@sfu.ca>
2019-02-19 20:03:36 -0800

    types: Fix formatting and styling

--- module/types/src/JsonType.ts
+++ module/types/src/JsonType.ts
@@ -8,7 +8,10 @@ import MathFlag from './MathFlag';
 /**
  * A JSON-serializable type.
  */
-type JsonType = {[key: string]: JsonType} | number | string | boolean | null;
+type JsonType = {[key: string]: JsonType} | number | string | boolean | null | JsonArray;
+
+// tslint:disable:array-type
+type JsonArray = Array<any> | {[key: number]: JsonType};
 
 // ---------------------------------------------------------------------------------------------------------------------
 export default JsonType;
