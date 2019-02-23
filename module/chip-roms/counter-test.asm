56ffd27c4e4903d2a1f3eb02858934371d71dbc8
Kyle Saburao <ksaburao@sfu.ca>
2019-02-22 19:51:47 -0800

    chip-roms: Add work on driving sim and counter test

---
+++ module/chip-roms/counter-test.asm
@@ -0,0 +1,69 @@
+DEFINE VALUE V3
+DEFINE H_X VA
+DEFINE T_X VB
+DEFINE O_X VC
+DEFINE V_Y VD
+
+init:
+    LD VALUE, #0
+    LD V0, #5
+    LD V1, #19 ; Hundreds x-coordinate
+
+    LD H_X, V1
+    ADD V1, V0
+    LD T_X, V1
+    ADD V1, V0
+    LD O_X, V1
+    LD V_Y, #D
+    JP loop
+
+
+loop:
+    LD I, BCD_STORE
+    LD B, VALUE
+
+    ADD VALUE, #1
+
+    CLS
+
+    LD V2, [I]
+
+    LD F, V0
+    DRW H_X, V_Y, #5
+
+    LD F, V1
+    DRW T_X, V_Y, #5
+
+    LD F, V2
+    DRW O_X, V_Y, #5
+
+
+    LD V0, VALUE
+    LD V1, #1
+    SUB V0, V1
+    SE V0, #FF
+    JP no_sound
+    
+    sound:
+        LD V0, #4
+        LD ST, V0
+    no_sound:
+        JP after_sound
+
+    after_sound:
+
+    LD V0, #4
+    LD DT, V0
+    CALL wait
+    JP loop
+
+
+wait:
+    waitLoop:
+        LD V0, DT
+        SE V0, #0
+        JP waitLoop
+    RET
+
+BCD_STORE:
+    db #0, #0, #0
