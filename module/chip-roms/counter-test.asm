eba0676cb7645f7c71a6fe8e9061c7251bb8367d
Kyle Saburao <ksaburao@sfu.ca>
2019-02-22 19:58:38 -0800

    chip-roms: Update counter test for general function use

--- module/chip-roms/counter-test.asm
+++ module/chip-roms/counter-test.asm
@@ -3,37 +3,18 @@ DEFINE H_X VA
 DEFINE T_X VB
 DEFINE O_X VC
 DEFINE V_Y VD
+DEFINE VALUE_TOP_LEFT_X #19
+DEFINE VALUE_TOP_LEFT_Y #D
 
 init:
     LD VALUE, #0
-    LD V0, #5
-    LD V1, #19 ; Hundreds x-coordinate
-
-    LD H_X, V1
-    ADD V1, V0
-    LD T_X, V1
-    ADD V1, V0
-    LD O_X, V1
-    LD V_Y, #D
+    CALL initValueDisplay
     JP loop
 
 
 loop:
-    LD I, BCD_STORE
-    LD B, VALUE
-
     CLS
-
-    LD V2, [I]
-
-    LD F, V0
-    DRW H_X, V_Y, #5
-
-    LD F, V1
-    DRW T_X, V_Y, #5
-
-    LD F, V2
-    DRW O_X, V_Y, #5
+    CALL renderValueDisplay
 
     SE VALUE, #FF
     JP no_sound
@@ -52,6 +33,32 @@ loop:
     LD DT, V0
     CALL wait
     JP loop
+
+initValueDisplay:
+    LD V0, #5
+    LD V1, VALUE_TOP_LEFT_X ; Hundreds x-coordinate
+    LD H_X, V1
+    ADD V1, V0
+    LD T_X, V1
+    ADD V1, V0
+    LD O_X, V1
+    LD V_Y, VALUE_TOP_LEFT_Y
+    RET
+
+renderValueDisplay:
+    LD I, BCD_STORE
+    LD B, VALUE
+    LD V2, [I]
+
+    LD F, V0
+    DRW H_X, V_Y, #5
+
+    LD F, V1
+    DRW T_X, V_Y, #5
+
+    LD F, V2
+    DRW O_X, V_Y, #5
+    RET
 
 wait:
     waitLoop:
