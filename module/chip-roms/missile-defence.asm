25b83efd432c1a60dc6ce00b331e2dd4c0370bcd
Kyle Saburao <ksaburao@sfu.ca>
2019-03-02 10:36:57 -0800

    chip-roms: Update MD notice

--- module/chip-roms/missile-defence.asm
+++ module/chip-roms/missile-defence.asm
@@ -1,5 +1,5 @@
 ; -------------------------------------------------------------------------------------------------------------------- ;
-; Chip-8 DVD Game                                                                                                      ;
+; Chip-8 Missile Defence                                                                                                      ;
 ; Copyright (C) 2019 Kyle Saburao                                                                                      ;
 ; - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -;
 ; This program was designed to be assembled with https://github.com/wernsey/chip8                                      ;
