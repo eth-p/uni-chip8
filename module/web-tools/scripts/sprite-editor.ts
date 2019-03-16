0003a1091e8677629bf1e8e5ee0ee720a6eeda3d
Kyle Saburao <ksaburao@sfu.ca>
2019-03-16 14:34:23 -0700

    web-tools: Add initial functioning sprite maker

--- module/web-tools/scripts/sprite-editor.ts
+++ module/web-tools/scripts/sprite-editor.ts
@@ -2,19 +2,43 @@
 //! Copyright (C) 2019 Team Chipotle
 //! MIT License
 //! --------------------------------------------------------------------------------------------------------------------
-// import dom_ready from '@chipotle/web/dom_ready';
+import dom_ready from '@chipotle/web/dom_ready';
 // YOUR CODE HERE
 
 import SpriteRegion from './SpriteRegion';
 
 function main(): void {
 	let region: SpriteRegion = new SpriteRegion();
-	for (let i = 0; i < SpriteRegion.COLUMNS; ++i) {
-		region.setPixel(i, i, true);
-	}
+	let regionTable: HTMLTableElement = <HTMLTableElement>document.querySelector('#region');
+	let output: HTMLTextAreaElement = <HTMLTextAreaElement>document.querySelector('#output');
 
-	console.log(region.getData());
+	output.readOnly = true;
+
+	regionTable.addEventListener('click', e => {
+		let tableData: HTMLTableDataCellElement = <HTMLTableDataCellElement>e.target;
+		let tableRow: HTMLTableRowElement = <HTMLTableRowElement>tableData.closest('tr');
+
+		if (tableRow && tableData) {
+			let column: number = tableData.cellIndex;
+			let row: number = tableRow.rowIndex;
+			let isOn: boolean = tableData.classList.contains('on');
+
+			if (isOn) {
+				tableData.classList.add('off');
+				tableData.classList.remove('on');
+			} else {
+				tableData.classList.add('on');
+				tableData.classList.remove('off');
+			}
+
+			isOn = !isOn;
+
+			region.setPixel(column, row, isOn);
+			if (output !== null) {
+				output.value = region.getData().toString();
+			}
+		}
+	});
 }
 
-main();
-// dom_ready(main);
+dom_ready(main);
